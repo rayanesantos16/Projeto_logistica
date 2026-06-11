17c045e5f21df2046355f28d0fda9b078e39bafa
Rayane Yasmin Dos Santos <rayaneyasmind@gmail.com>
2026-06-11 12:07:31 -0300

    criei a def inicial de adicionar produtos

--- Projeto-Logística/versão1.py
+++ Projeto-Logística/versão1.py
@@ -1,12 +1,10 @@
-estoque =  []
+estoque = []
 
 
-def produto_por_id(id_produto):
-    for produto in estoque:
-        
-        
-        
-        
-        
-        print("")
-
+def adicionar_produto():
+    print("\n--- ADICIONAR PRODUTO ---")
+    id_produto = int(input("ID: "))
+    nome_produto = input("Nome: ")
+    quantidade_produto = int(input("Quantidade: "))
+    local_produto = input("Localização: ")
+    
