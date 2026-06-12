0661366738db73fc813d76a697dd57cf33075eeb
Rayane Yasmin Dos Santos <rayaneyasmind@gmail.com>
2026-06-12 10:47:13 -0300

    menu principal colocado

--- Projeto-Logística/versão1.py
+++ Projeto-Logística/versão1.py
@@ -3,23 +3,23 @@ estoque = []
 def adicionar_produto():
     print("\n===== ADICIONAR PRODUTO =====")
     produto_id = int(input("Digite o ID do produto (apenas números): "))
-    
+
     id_existe = False
     for produto in estoque:
         if produto[0] == produto_id:
             id_existe = True
 
-        if id_existe:
-            print("Já existe um produto com este ID.")
-        else:
-             nome = input("Digite o nome do produto: ")
-             quantidade = int(input("Digite a quantidade de produtos: "))
-             localizacao = input("Digite a localização do produto: ")
+    if id_existe:
+        print("Já existe um produto com este ID.")
+    else:
+        nome = input("Digite o nome do produto: ")
+        quantidade = int(input("Digite a quantidade de produtos: "))
+        localizacao = input("Digite a localização do produto: ")
+
 
         novo_produto = [produto_id, nome, quantidade, localizacao]
         estoque.append(novo_produto)
         print("Sucesso: Produto cadastrado com êxito!")
-
 def listar_produtos():
     print("\n===== LISTA DE PRODUTOS =====")
     if len(estoque) == 0:
@@ -30,12 +30,10 @@ def listar_produtos():
 
         for p in estoque:
             print(f"{p[0]:<9} | {p[1]:<25} | {p[2]:<10} | {p[3]:<15}")
-
-
+    
 def buscar_produto():
     print("\n===== BUSCAR PRODUTO POR ID =====")
     produto_id = int(input("Digite o ID do produto que deseja buscar: "))
-
     achou = False
     for produto in estoque:
         if produto[0] == produto_id:
@@ -48,7 +46,7 @@ def buscar_produto():
 
     if not achou:
         print("Produto não está no estoque.")
-
+    
 def atualizar_estoque():
     print("\n===== ADICIONAR AO ESTOQUE =====")
     produto_id = int(input("Digite o ID do produto: "))
@@ -65,22 +63,19 @@ def atualizar_estoque():
             quantidade_p = produto_removido[2]
             local_produto = produto_removido[3]
 
-            print(f"Produto selecionado: {nome_produto} (Quantidade atual: {quantidade_p}i)ade")
+            print(f"Produto selecionado: {nome_produto} (Quantidade atual: {quantidade_p})")
             print("1. Entrada (+)")
             print("2. Saída (-)")
             opcao = input("Escolha uma operação: ")
             quantidade_alterar = int(input("Digite a quantidade: "))
 
-            sucesso = False
             if opcao == '1':
                 quantidade_p = quantidade_p + quantidade_alterar
                 print(f"Sucesso! Nova quantidade: {quantidade_p}")
-                sucesso = True
             elif opcao == '2':
                 if quantidade_p >= quantidade_alterar:
                     quantidade_p = quantidade_p - quantidade_alterar
                     print(f"Sucesso! Nova quantidade: {quantidade_p}")
-                    sucesso = True
                 else:
                     print("Erro: Estoque insuficiente.")
             else:
@@ -92,3 +87,29 @@ def atualizar_estoque():
     if not achou:
         print("Produto não esta no estoque.")
 
+def travamenu():
+    input("\nPressione <ENTER> para continuar...")
+
+print("Bem vindo ao menu!")
+while True:
+    print("\nPor favor, selecione uma opção:")
+    print(" 1- Adicionar produto \n| 2- listar produto \n| 3- Buscar produto \n| 4- Atualizar estoque \n| 5- Sair")
+    opção = input("Escolha: ")
+    
+    if opção == "1":
+        adicionar_produto()
+        travamenu()
+    elif opção == "2":
+        listar_produtos()
+        travamenu()
+    elif opção == "3":
+        buscar_produto()
+        travamenu()
+    elif opção == "4":
+        atualizar_estoque()
+        travamenu()
+    elif opção == "5": 
+        print("Menu encerrado...")
+        break
+    else: 
+        print("Opção inválida! Tente novamente.")
