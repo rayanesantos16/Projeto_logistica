0a74ad02f0e26efd3d5cab8ec3612641bac52856
Rayane Yasmin Dos Santos <rayaneyasmind@gmail.com>
2026-06-12 11:25:32 -0300

    Update versão1.py
    pag errada

--- Projeto-Logística/versão1.py
+++ Projeto-Logística/versão1.py
@@ -1,10 +1,1 @@
-estoque = []
 
-
-def adicionar_produto():
-    print("\n--- ADICIONAR PRODUTO ---")
-    id_produto = int(input("ID: "))
-    nome_produto = input("Nome: ")
-    quantidade_produto = int(input("Quantidade: "))
-    local_produto = input("Localização: ")
-    
