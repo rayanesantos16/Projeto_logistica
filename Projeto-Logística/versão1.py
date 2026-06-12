5eca67ded5b670718adb3b487b74165586f88013
Rayane Yasmin Dos Santos <rayaneyasmind@gmail.com>
2026-06-12 09:49:22 -0300

    adicionada a busca de produtos

--- Projeto-Logística/versão1.py
+++ Projeto-Logística/versão1.py
@@ -10,7 +10,7 @@ def adicionar_produto():
             id_existe = True
 
         if id_existe:
-            print("Erro: Já existe um produto com este ID.")
+            print("Já existe um produto com este ID.")
         else:
              nome = input("Digite o nome do produto: ")
              quantidade = int(input("Digite a quantidade de produtos: "))
@@ -21,12 +21,30 @@ def adicionar_produto():
         print("Sucesso: Produto cadastrado com êxito!")
 
 def listar_produtos():
-    print("\n--- LISTA DE PRODUTOS ---")
+    print("\n===== LISTA DE PRODUTOS =====")
     if len(estoque) == 0:
         print("O estoque está vazio.")
     else:
         print("ID        | Nome                      | Qtd        | Localização    ")
-        print("--------------------------------------------------------------------")
+        print("====================================================================")
 
         for p in estoque:
             print(f"{p[0]:<9} | {p[1]:<25} | {p[2]:<10} | {p[3]:<15}")
+
+
+def buscar_produto():
+    print("\n===== BUSCAR PRODUTO POR ID =====")
+    produto_id = int(input("Digite o ID do produto que deseja buscar: "))
+
+    achou = False
+    for produto in estoque:
+        if produto[0] == produto_id:
+            print("\n===== Produto Encontrado =====")
+            print(f"ID: {produto[0]}")
+            print(f"Nome: {produto[1]}")
+            print(f"Quantidade: {produto[2]}")
+            print(f"Localização: {produto[3]}")
+            achou = True
+
+    if not achou:
+        print("Produto não está no estoque.")
